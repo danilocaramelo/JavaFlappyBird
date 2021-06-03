cada0d9bf3e190933306ebee736036ebbe11d8fa
matheus-maia <matheus.carvalho@jointecnologia.com.br>
2021-06-03 15:36:40 -0300

    pipes logic
    Signed-off-by: matheus-maia <matheus.carvalho@jointecnologia.com.br>

--- src/main/java/br/ucsal/flappybird/Jogo.java
+++ src/main/java/br/ucsal/flappybird/Jogo.java
@@ -1,12 +1,14 @@
 package br.ucsal.flappybird;
 
 import java.awt.Color;
+
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Label;
 import java.awt.geom.AffineTransform;
 import java.awt.image.AffineTransformOp;
+import java.util.Random;
 import java.awt.Image;
 
 import javax.swing.ImageIcon;
@@ -26,7 +28,7 @@ public class Jogo extends JPanel {
 		super.paintComponent(g);
 		desenhar(g);
 	}
-
+	
 	private void desenhar(Graphics g) {
 		Graphics2D g2d = (Graphics2D) g;
 		ImageIcon i = new ImageIcon(getClass().getResource("/bird.png"));
@@ -39,18 +41,28 @@ public class Jogo extends JPanel {
 		double rotationTo180 = Math.toRadians (180);
 		double rotationTo45 = Math.toRadians (45);
 		
+		int topPipeHeight = 0;
+		int bottomPipeHeight = 0;
+		int sum = 0;
+				
+		do {
+			topPipeHeight = (int) (Math.random() * (h - 0)) + 0;
+			bottomPipeHeight = (int) (Math.random() * (h - 0)) + 0;
+			sum = (h - topPipeHeight) + (h - bottomPipeHeight);
+			
+		} while ((h - sum) > 284 || (h -sum) < 190);
+		
+		
 		AffineTransform rotate180 = AffineTransform.getRotateInstance(rotationTo180, 200, 400);
 		AffineTransform rotate45 = AffineTransform.getRotateInstance(rotationTo45, 200, 400);
 		AffineTransform rotate0 = g2d.getTransform();
 		
 		g2d.setTransform(rotate180);
-		g2d.drawImage(pipe, -700, 500, w/7, h, null);
+		g2d.drawImage(pipe, -700, (topPipeHeight), w/8, h, null);
 		
 		g2d.setTransform(rotate0);
-		g2d.drawImage(pipe, 930, 280, w/7, h, null);
-		g2d.setTransform(rotate45);
+		g2d.drawImage(pipe, 950, (bottomPipeHeight), w/8, h, null);
 		g2d.drawImage(bird, 60,280,90,90,null);
-//		g2d.drawImage(pipe, 400, (h - 500), 130, 500, null);
 		
 	}
 }
