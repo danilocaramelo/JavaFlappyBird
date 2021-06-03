0d66f606229fec6b73f12181d88c8b8854d781c7
matheus-maia <matheus.carvalho@jointecnologia.com.br>
2021-06-03 13:32:29 -0300

    set the transform to rotate images
    Signed-off-by: matheus-maia <matheus.carvalho@jointecnologia.com.br>

--- src/main/java/br/ucsal/flappybird/Jogo.java
+++ src/main/java/br/ucsal/flappybird/Jogo.java
@@ -15,12 +15,12 @@ import javax.swing.JPanel;
 public class Jogo extends JPanel {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	public Jogo() {
 		this.add(new Label());
 		setBackground(Color.decode("#018695"));
 	}
-	
+
 	@Override
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
@@ -36,13 +36,19 @@ public class Jogo extends JPanel {
 		Dimension size = getSize();
 		int w = (int) size.getWidth();
 		int h = (int) size.getHeight();
+		double rotationTo180 = Math.toRadians (180);
+		double rotationTo45 = Math.toRadians (45);
 		
-		double rotationRequired = Math.toRadians (180);
-		AffineTransform a = AffineTransform.getRotateInstance(rotationRequired, 200, 400);
-		Graphics2D gBack = g2d;
-		gBack.setTransform(a);
-		gBack.drawImage(pipe, 200, 400, null);
+		AffineTransform rotate180 = AffineTransform.getRotateInstance(rotationTo180, 200, 400);
+		AffineTransform rotate45 = AffineTransform.getRotateInstance(rotationTo45, 200, 400);
+		AffineTransform rotate0 = g2d.getTransform();
 		
+		g2d.setTransform(rotate180);
+		g2d.drawImage(pipe, -700, 500, w/7, h, null);
+		
+		g2d.setTransform(rotate0);
+		g2d.drawImage(pipe, 930, 280, w/7, h, null);
+		g2d.setTransform(rotate45);
 		g2d.drawImage(bird, 60,280,90,90,null);
 //		g2d.drawImage(pipe, 400, (h - 500), 130, 500, null);
 		
