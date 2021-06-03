96e4bc448ccb0e731452118da66887fe7f92af80
Danilo Caramelo <danilo.caramelogit@gmail.com>
2021-06-03 13:00:15 -0300

    feature: insert start code game

---
+++ src/main/java/br/ucsal/flappybird/Menu.java
@@ -0,0 +1,62 @@
+package br.ucsal.flappybird;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.Label;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.plaf.FontUIResource;
+
+public class Menu extends JPanel{
+
+	private JButton play = new JButton("JOGAR"); 
+    private JButton comojogar = new JButton("COMO JOGAR");
+    private JButton resolucao = new JButton("RESOLUÇÃO");
+	
+	public Menu() {
+
+            //botões
+	     	this.setLayout(null);    	    	
+	     	play.setBackground(Color.GREEN);resolucao.setBackground(Color.GREEN);comojogar.setBackground(Color.GREEN);
+	     	play.setForeground(Color.WHITE);resolucao.setForeground(Color.WHITE);comojogar.setForeground(Color.WHITE);
+	     	play.setFont(new FontUIResource("SansSerif",Font.BOLD, 18));
+	     	comojogar.setFont(new FontUIResource("SansSerif",Font.BOLD, 18));
+	     	resolucao.setFont(new FontUIResource("SansSerif",Font.BOLD, 18));
+	     	
+	     	
+	     	//posição x e y e largura e altura
+	     	play.setBounds(325, 290, 540, 80);
+//	     	resolucao.setBounds(55, 120, 180, 40);
+//	     	comojogar.setBounds(55, 170, 180, 40);
+	     	//adicionar um boto
+	     	this.add(play);
+//	     	this.add(resolucao); 
+//	     	this.add(comojogar);
+	    
+	}
+	//imagens
+	@Override
+	protected void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		setDoubleBuffered(true);
+		desenhar(g);
+	}
+    //imagens
+	private void desenhar(Graphics g) {
+		
+		ImageIcon ii = new ImageIcon(getClass().getResource("/Menu.png"));
+		ImageIcon i = new ImageIcon(getClass().getResource("/nuveem.png"));
+		Image title = ii.getImage();
+		Image clouds = i.getImage();
+
+		g.drawImage(title, 0,-5,1200,200,null);
+	    g.drawImage(clouds, 0,190,1200,500,null);
+	}
+	
+	
+}
---
+++ src/main/java/br/ucsal/flappybird/Principal.java
@@ -0,0 +1,35 @@
+package br.ucsal.flappybird;
+
+import java.awt.Event;
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.Font;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.plaf.FontUIResource;
+
+    public class Principal extends JFrame{
+    	
+   
+    public Principal () {
+    	this.setTitle("FlappyBird"); // t�tulo do jogo
+    	this.setSize(1200,700); // tamanho da janela
+    	this.add(new Jogo()); //classe Menu
+    	this.setResizable(false); // maximizar janela (false) 
+    	
+    	this.setLocationRelativeTo(null); // inicializar janela no meio da tela 
+    	this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); /*Isso faz com que o aplicativo seja encerrado 
+    	quando o aplicativo recebe um evento de fechamento de janela do sistema operacional.*/
+    	
+    }
+	public static void main(String[] args) throws Exception {
+    	EventQueue.invokeLater(new Runnable() {
+    		public void run() {
+    			new Principal().setVisible(true);				
+    		}
+    	});
+     }
+  }
+    
+
+
